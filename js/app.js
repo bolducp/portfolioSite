b66045564174034c4dd5310ac93829c62ad39ef3
Paige Bolduc <paigebolduc@gmail.com>
2016-02-24 23:31:03 -0800

    add My Story section

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('siteApp', ['ui.router', 'duScroll']);
 
-app.value('duScrollDuration', 2000);
+app.value('duScrollDuration', 1200);
 
 app.controller('mainCtrl', function($scope, $location, $anchorScroll) {
    $scope.scrollTo = function(id) {
